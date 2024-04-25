46b43570e9b5f896a83cb5998e861d7e26d53667
Лиза <104992063+olegoloo@users.noreply.github.com>
2024-04-25 21:51:58 +0300

    Update __init__.py

--- spasibo/__init__.py
+++ spasibo/__init__.py
@@ -33,13 +33,13 @@ class Player(BasePlayer):
 # PAGES
 class WinnerPage(Page):
     def is_displayed(self):
-        return self.id_in_group == 1 or self.id_in_group == 8
+        return self.id_in_group == 1 or self.id_in_group == 3
     form_model = 'player'
     form_fields = ['get', 'contact']
 
 class NoWinPage(Page):
     def is_displayed(self):
-        return self.id_in_group != 1 and self.id_in_group != 8
+        return self.id_in_group != 1 and self.id_in_group != 3
     form_model = 'player'
 
 
